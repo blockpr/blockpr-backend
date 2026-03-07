fe96b948035badf0038697314aed2fce7c911e39
Facundo Girardi <blockprcode@gmail.com>
2026-03-07 02:21:23 -0300

    hash

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,8 @@
-from app.routes import auth, health, hash_test
+from app.routes import auth, health, hashes
 
 
 def register_routes(app):
     """Register all routes with the application"""
     app.register_blueprint(health.bp)
-    app.register_blueprint(hash_test.bp)
+    app.register_blueprint(hashes.bp)
     app.register_blueprint(auth.bp)
